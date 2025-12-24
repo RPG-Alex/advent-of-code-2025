6a975d21b5ea08662629a4d773b10131a1d80002
RPG-Alex <mralexblood@outlook.com>
2025-12-24 22:09:15 +0800

    day 2-1 complete

--- src/days/day2.rs
+++ src/days/day2.rs
@@ -1,4 +1,4 @@
-use std::{fs::File, io::Read};
+use std::{fs::File, i32, io::Read};
 
 fn main() {
     let part1 = part_1();
@@ -7,13 +7,43 @@ fn main() {
     println!("part 2: {}", part2);
 }
 
-fn part_1() -> i32 {
+fn part_1() -> i64 {
     let file = File::open("inputs/day2_1");
     let mut content = String::new();
     let _ = file
         .expect("failed to read file")
         .read_to_string(&mut content);
-    let mut total = 0;
+    let mut total: i64 = 0;
+    for entry in content.split(",") {
+        let mut id = entry.split("-");
+        let mut min: i64 = id.next().unwrap().parse().unwrap();
+        let max: i64 = id.next().unwrap().trim().parse().unwrap();
+
+        while min <= max {
+            let num = min.to_string();
+            if num.len() % 2 == 0 {
+                let (begin,end) = num.split_at(num.len()/2);
+                let (mut b_match, mut e_match) = (String::new(), String::new());
+                for (i, c) in begin.chars().enumerate() {
+                    if let Some(end_c) = end.chars().nth(i) {
+                        if c == end_c {
+                            b_match.push(c);
+                            e_match.push(end_c);
+                        } else {
+                            b_match.clear();
+                            e_match.clear();
+                            break;
+                        }
+                    }
+                }
+                if b_match == e_match && !b_match.is_empty() && !e_match.is_empty(){
+                    total += min;
+                }
+            }
+            min += 1;
+        }
+    }
+
 
     total
 }
